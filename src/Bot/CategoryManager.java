844bacc8803dbe580309a53851610b0ef3eea345
Konstantin Kolmogortsev <mr.kolmogortsev@gmail.com>
2021-09-13 19:49:02 +0500

    CategoryManager.java
    changed way to add category

--- src/Bot/CategoryManager.java
+++ src/Bot/CategoryManager.java
@@ -7,18 +7,21 @@ import java.util.HashSet;
 public class CategoryManager {
     private String name;
     private ArrayList<Category> categories;
+    private Account masterAccount;
 
-    public CategoryManager(String name){
+    public CategoryManager(String name, Account masterAccount){
         this.name = name;
         categories = new ArrayList<>();
+        this.masterAccount = masterAccount;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public void addCategory(Category cat){
-        categories.add(cat);
+    public void addCategory(String name){
+        var newCategory = new Category(name,0,this);
+        addCategory(newCategory);
     }
 
     public void removeCategory(Category cat){
@@ -37,5 +40,9 @@ public class CategoryManager {
     @Override
     public String toString() {
         return name;
+    }
+
+    private void addCategory(Category cat){
+        categories.add(cat);
     }
 }
