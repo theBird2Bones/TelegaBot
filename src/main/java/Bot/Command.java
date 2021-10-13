c8b308d9cd0a915f1bbb727fd2d4929f7b459cdd
Konstantin Kolmogortsev <mr.kolmogortsev@gmail.com>
2021-10-13 14:31:35 +0500

    worked on commands. not final. need to fix

--- src/main/java/Bot/Command.java
+++ src/main/java/Bot/Command.java
@@ -1,38 +1,61 @@
 package Bot;
 
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import java.util.function.Function;
 
+interface Command {
+    String execute(StateManager stateManager);
+}
 
+//class InnerContentProcessor implements Command{
+//
+//    @Override
+//    public String execute(StateManager stateManager, String message) {
+//        return switch (stateManager.getCurrentState()){
+//            case CurrentState.tookAccount -> {
+//                var indexOfCategoryManager = message.split(" ")[2];
+//                stateManager.setTakenCategoryManager();
+//            }
+//        }
+//    }
+//}
+//
+//class CategoryManagerProcessor implements Command{
+//    private Function<CategoryManager, String> processor;
+//
+//    public CategoryManagerProcessor(Function<CategoryManager, String> processor){
+//        this.processor = processor;
+//    }
+//
+//    @Override
+//    public String execute(StateManager stateManager) {
+//        if(stateManager.getCurrentState() == CurrentState.tookCategoryManager)
+//            return processor.apply(stateManager.getTakenCategoryManager());
+//        return "You should choose one of the category managers first!";
+//    }
+//}
+//
+//class CategoryProcessor implements Command{
+//    private Category category;
+//    private Function<Category, String> processor;
+//    public CategoryProcessor(Category category, Function<Category, String> processor){
+//        this.category= category;
+//        this.processor = processor;
+//    }
+//
+//    @Override
+//    public String execute(StateManager stateManager) {
+//        return processor.apply(category);
+//    }
+//}
+//
+class TextProcessor implements Command{
+    private String message;
 
-public class Command {
-    public enum Name{
-        about,
-        help,
-        getAccountTotal,
-        getInnerCategoryManagers,
-        getInnerCategories,
-        getCategoryManagerTotal,
-        getCategoryTotal,
-        addCategory,
-
-    }
-    private final Name name;
-
-    private SendMessage message;
-
-    public Command(Name name, String sendMessageText) {
-        this.name = name;
-
-        var message = new SendMessage();
-        message.setText(sendMessageText);
+    public TextProcessor(String message){
         this.message = message;
     }
-
-    public Name getName() {
-        return name;
-    }
-
-    public SendMessage getMessage() {
+    @Override
+    public String execute(StateManager stateManager) {
         return message;
     }
 }
