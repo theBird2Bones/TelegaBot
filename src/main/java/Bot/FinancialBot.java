c93930d42e61c232a0ffb42118f219eca1947c68
Konstantin Kolmogortsev <mr.kolmogortsev@gmail.com>
2021-10-18 16:05:09 +0500

    added user input processing

--- src/main/java/Bot/FinancialBot.java
+++ src/main/java/Bot/FinancialBot.java
@@ -5,6 +5,7 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import  org.telegram.telegrambots.meta.api.objects.*;
 import java.util.*;
+import java.util.function.Function;
 import java.util.regex.Pattern;
 
 public class FinancialBot extends TelegramLongPollingBot {
@@ -30,8 +31,9 @@ public class FinancialBot extends TelegramLongPollingBot {
     }
 
     private HashMap<String, StateManager> usersStateManager = new HashMap<>();
-    private HashMap<String, Command> commands = createCommands();
+    private HashMap<String, Function<StateManager,Command>> commands = createCommands();
 
+    //как создать команды заранее, если они требуют доп параметры???? я не понимаю
     private StateManager initStateManagerWithId(String chatId){
         var stateManager = new StateManager("Common", chatId);
         stateManager.getTakenAccount().addCategoryManager("Income");
@@ -39,123 +41,47 @@ public class FinancialBot extends TelegramLongPollingBot {
         return stateManager;
     }
 
-    private HashMap<String, Command> createCommands(){
-        var commands = new HashMap<String, Command>();
-        commands.put("/about", new TextProcessor( "Creators: Konstantin & Mariia"));
-        commands.put("/help", new TextProcessor("That I can do:\n/about -" +
-                " Show my creators\n/help - Show the list of possible commands"));
-//        commands.put("get total",
-//                new AccountProcessor(stateManager -> Formatter.formatAccountInnerCategoryManagerTotal(stateManager)));
-//        commands.put("show content",
-//                new AccountProcessor(stateManager -> Formatter.formatAccountInnerCategoryManager(stateManager)));
-//        commands.put("move to", new TransitionProcessorFromAccountToCategoryManager("to take a category manager you should input its index"));
-        //проблема: для прописывания таких команд надо иметь состояния диалога
+    private HashMap<String, Function<StateManager,Command>> createCommands(){
+        var commands = new HashMap<String, Function<StateManager,Command>>();
 
         return commands;
     }
 
     //TODO: there is ForceReply for chain conversation. it worth to check it
-    private SendMessage getResponseToInputtedMessage(Message message){
-        if(!usersStateManager.containsKey(message.getChatId().toString())){
-           usersStateManager.put(message.getChatId().toString(),
-                   initStateManagerWithId(message.getChatId().toString()));
+    private SendMessage getResponseToInputtedMessage(Message message) {
+        if (!usersStateManager.containsKey(message.getChatId().toString())) {
+            usersStateManager.put(message.getChatId().toString(),
+                    initStateManagerWithId(message.getChatId().toString()));
         }
         var userStateManager = usersStateManager.get(message.getChatId().toString());
 
-        var messageText = message.getText();
+        var messageText = message.getText().toLowerCase();
+        var outMessage = "undefined command";
 
-        if(messageText.contains("/about")){
-            return MessageBuilder.createMessage("Creators: Konstantin & Mariia", message.getChatId().toString());
+        if (messageText.contains("/about")) {
+            outMessage = new AboutCommand(userStateManager).execute();
+        } else if (messageText.contains("/help")) {
+            outMessage = new HelpCommand(userStateManager).execute();
+        } else if (messageText.contains("show content")) {
+            outMessage = new ShowContentCommand(userStateManager).execute();
+        } else if (messageText.contains("get total")) {
+            outMessage = new GetTotalCommand(userStateManager).execute();
+        } else if (messageText.contains("move to")) {
+            outMessage = new MoveToCommand(userStateManager, messageText).execute();
+        } else if (messageText.contains("increase")) {
+            outMessage = new IncreaseCommand(userStateManager, messageText).execute();
+        } else if (messageText.contains("create")) {
+            outMessage = new CreateCommand(userStateManager, messageText).execute();
+        } else if (messageText.contains("move up")) {
+            outMessage = new MoveUpCommand(userStateManager).execute();
+        } else if (messageText.contains("delete")) {
+            outMessage = new DeleteCommand(userStateManager, messageText).execute();
+        } else if (messageText.contains("rename")) {
+            outMessage = new RenameCommand(userStateManager, messageText).execute();
         }
-        else if(messageText.contains("/help")) {
-            return MessageBuilder.createMessage("That I can do:\n/about -" +
-                    " Show my creators\n/help - Show the list of possible commands", message.getChatId().toString());
-        }
-        else if(messageText.contains("show inner")){
-            if (userStateManager.getCurrentState() == CurrentState.tookAccount)
-                return MessageBuilder.createMessage(
-                        Formatter.formatAccountInnerCategoryManager(userStateManager.getTakenAccount()),
-                        message.getChatId().toString());
-            else if (userStateManager.getCurrentState() == CurrentState.tookCategoryManager)
-                return MessageBuilder.createMessage(
-                        Formatter.formatCategoryManagerContent(userStateManager.getTakenCategoryManager()),
-                        message.getChatId().toString());
-        }
-        else if(messageText.contains("get total")){
-            if (userStateManager.getCurrentState() == CurrentState.tookAccount)
-                return MessageBuilder.createMessage(
-                        Formatter.formatAccountInnerCategoryManagerTotal(userStateManager.getTakenAccount()),
-                        message.getChatId().toString());
-            else if (userStateManager.getCurrentState() == CurrentState.tookCategoryManager)
-                return MessageBuilder.createMessage(
-                        Formatter.formatCategoryManagerTotal(userStateManager.getTakenCategoryManager()),
-                        message.getChatId().toString());
-        }
-
-        else if(messageText.contains("move to")){
-            if (userStateManager.getCurrentState() == CurrentState.tookAccount) {
-                userStateManager.setCurrentState(CurrentState.tookCategoryManager);
-                userStateManager.setTakenCategoryManager(
-                        userStateManager
-                                .getTakenAccount()
-                                .getCategoryManagers()
-                                .get(Integer.parseInt(messageText.split(" ")[2]))
-                );
-                return MessageBuilder.createMessage(
-                        String.format("you moved to %s\n%s",
-                                userStateManager.getTakenCategoryManager().getName(),
-                                Formatter.formatCategoryManagerContent(
-                                        userStateManager.getTakenCategoryManager())),
-                        message.getChatId().toString());
-            }
-        }
-        else if(messageText.contains("add at")){
-            if(userStateManager.getCurrentState() == CurrentState.tookCategoryManager){
-                var splittedMessageText = messageText.split(" ");
-                if(Pattern.matches("^-?\\d+?$",splittedMessageText[2])){
-                    userStateManager
-                            .getTakenCategoryManager()
-                            .getCategories()
-                            .get(Integer.parseInt(splittedMessageText[2]))
-                            .add(Long.parseLong(splittedMessageText[3]));
-                }
-            }
-
-            return MessageBuilder.createMessage(
-                    Formatter.formatCategoryManagerContent(userStateManager.getTakenCategoryManager()),
-                    userStateManager.getChatID()
-            );
-        }
-        else if(messageText.contains("add")) {
-            if (userStateManager.getCurrentState() == CurrentState.tookCategoryManager) {
-                var splittedMessageText = messageText.split(" ");
-                userStateManager
-                        .getTakenCategoryManager()
-                        .addCategory(splittedMessageText[1]);
-            }
-            return MessageBuilder.createMessage(
-                    Formatter.formatCategoryManagerContent(userStateManager.getTakenCategoryManager()),
-                    userStateManager.getChatID()
-            );
-        }
-        else if(messageText.contains("move up")){
-
-            if (userStateManager.getCurrentState() == CurrentState.tookCategoryManager) {
-                userStateManager.releaseCategoryManager();
-            }
-            return MessageBuilder.createMessage(
-                    Formatter.formatAccountInnerCategoryManager(userStateManager.getTakenAccount()),
-                    userStateManager.getChatID()
-            );
-        }
-
-        var sM = new SendMessage(message.getChatId().toString(), "hehe");
-        return sM;
+        return MessageBuilder.createMessage(outMessage, userStateManager.getChatID());
     }
 
-    private void prepareMessage(Message message, CurrentState state){
-
-    }
     private static class MessageBuilder{
         public static SendMessage createMessage(String messageText, String chatId){
             return new SendMessage(chatId,messageText);
