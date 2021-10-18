5a7deaf95c8ad2d10b6329e3ff8f194231f5dde4
Konstantin Kolmogortsev <mr.kolmogortsev@gmail.com>
2021-10-18 16:04:22 +0500

    added Command pattern

--- src/main/java/Bot/Command.java
+++ src/main/java/Bot/Command.java
@@ -1,61 +1,197 @@
 package Bot;
 
-import java.util.function.Function;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
-interface Command {
-    String execute(StateManager stateManager);
+abstract class Command {
+    protected StateManager stateManager;
+    public Command(StateManager stateManager){
+        this.stateManager = stateManager;
+    }
+    abstract public String execute();
 }
 
-//class InnerContentProcessor implements Command{
-//
-//    @Override
-//    public String execute(StateManager stateManager, String message) {
-//        return switch (stateManager.getCurrentState()){
-//            case CurrentState.tookAccount -> {
-//                var indexOfCategoryManager = message.split(" ")[2];
-//                stateManager.setTakenCategoryManager();
-//            }
-//        }
-//    }
-//}
-//
-//class CategoryManagerProcessor implements Command{
-//    private Function<CategoryManager, String> processor;
-//
-//    public CategoryManagerProcessor(Function<CategoryManager, String> processor){
-//        this.processor = processor;
-//    }
-//
-//    @Override
-//    public String execute(StateManager stateManager) {
-//        if(stateManager.getCurrentState() == CurrentState.tookCategoryManager)
-//            return processor.apply(stateManager.getTakenCategoryManager());
-//        return "You should choose one of the category managers first!";
-//    }
-//}
-//
-//class CategoryProcessor implements Command{
-//    private Category category;
-//    private Function<Category, String> processor;
-//    public CategoryProcessor(Category category, Function<Category, String> processor){
-//        this.category= category;
-//        this.processor = processor;
-//    }
-//
-//    @Override
-//    public String execute(StateManager stateManager) {
-//        return processor.apply(category);
-//    }
-//}
-//
-class TextProcessor implements Command{
-    private String message;
-
-    public TextProcessor(String message){
-        this.message = message;
+class AboutCommand extends Command {
+    public AboutCommand(StateManager stateManager){
+        super(stateManager);
     }
     @Override
-    public String execute(StateManager stateManager) {
-        return message;
+    public String execute() {
+        return "Creators: Konstantin & Mariia";
+    }
+}
+
+class HelpCommand extends Command{
+    public HelpCommand(StateManager stateManager){
+        super(stateManager);
+    }
+
+    @Override
+    public String execute() {
+        return "That I can do:\n/about - Show my creators\n/help - Show the list of possible commands";
+    }
+}
+
+class ShowContentCommand extends Command{
+    public ShowContentCommand(StateManager stateManager){
+        super(stateManager);
+    }
+
+    @Override
+    public String execute() {
+        return switch (stateManager.getCurrentState()){
+            case tookAccount ->
+                Formatter.formatAccountInnerCategoryManager(stateManager.getTakenAccount());
+            case tookCategoryManager ->
+                    Formatter.formatCategoryManagerContent(stateManager.getTakenCategoryManager());
+        };
+    }
+}
+
+class GetTotalCommand extends Command{
+    public GetTotalCommand(StateManager stateManager){
+        super(stateManager);
+    }
+
+    @Override
+    public String execute() {
+        return switch (stateManager.getCurrentState()){
+            case tookAccount ->
+                    Formatter.formatAccountInnerCategoryManagerTotal(stateManager.getTakenAccount());
+            case tookCategoryManager ->
+                   Formatter.formatCategoryManagerTotal(stateManager.getTakenCategoryManager());
+        };
+    }
+}
+
+class MoveToCommand extends Command{
+    private String command;
+    public MoveToCommand(StateManager stateManager, String command){
+        super(stateManager);
+        this.command = command;
+    }
+
+    @Override
+    public String execute() {
+        return switch (stateManager.getCurrentState()){
+            case tookAccount -> {
+                stateManager.setCurrentState(CurrentState.tookCategoryManager);
+                stateManager.setTakenCategoryManager(
+                        stateManager .getTakenAccount() .getCategoryManagers()
+                                .get(Integer.parseInt(command.split(" ")[2])-1));
+                yield  String.format("You are moved to %s",
+                        stateManager .getTakenAccount() .getCategoryManagers()
+                                .get(Integer.parseInt(command.split(" ")[2])-1));
+            }
+            case tookCategoryManager -> "Please, move up to choose 'Income' or 'Outcome' category";
+        };
+    }
+}
+
+class IncreaseCommand extends Command {
+    private String command;
+
+    public IncreaseCommand(StateManager stateManager, String command) {
+        super(stateManager);
+        this.command = command;
+    }
+
+    @Override
+    public String execute() {
+        return switch (stateManager.getCurrentState()) {
+            case tookAccount -> "Please choose 'Income' or 'Outcome' category to change balance";
+            case tookCategoryManager -> {
+                var splittedMessageText = command.split(" ");
+                if (Pattern.matches("^-?\\d+?$", splittedMessageText[2])) {
+                    stateManager.getTakenCategoryManager().getCategories()
+                            .get(Integer.parseInt(splittedMessageText[1]) - 1)
+                            .add(Long.parseLong(splittedMessageText[2]));
+                    yield Formatter.formatCategoryManagerTotal(stateManager.getTakenCategoryManager());
+                }
+                yield "Sorry but you have to write digits only";
+            }
+        };
+    }
+}
+
+class CreateCommand extends Command{
+    private String command;
+    public CreateCommand(StateManager stateManager, String command){
+        super(stateManager);
+        this.command = command;
+    }
+
+    @Override
+    public String execute() {
+        return switch (stateManager.getCurrentState()){
+            case tookAccount -> "Sorry, but you can create categories in 'Income' or 'Outcome' only";
+            case tookCategoryManager ->{
+                var categoryName = Arrays.stream(command.split(" "))
+                        .skip(1).collect(Collectors.joining(" ")).toString();
+                stateManager.getTakenCategoryManager().addCategory(categoryName);
+                yield Formatter.formatCategoryManagerContent(stateManager.getTakenCategoryManager());
+            }
+        };
+    }
+}
+
+class MoveUpCommand extends Command{
+    public MoveUpCommand(StateManager stateManager){
+        super(stateManager);
+    }
+
+    @Override
+    public String execute() {
+        stateManager.releaseCategoryManager();
+        stateManager.setCurrentState(CurrentState.tookAccount);
+        return String.format("You moved up to %s", stateManager.getTakenAccount());
+    }
+}
+
+class DeleteCommand extends Command{
+    private String command;
+    public DeleteCommand(StateManager stateManager, String command){
+        super(stateManager);
+        this.command = command;
+    }
+
+    @Override
+    public String execute() {
+        var splitterCommand = command.split(" ");
+        return switch (stateManager.getCurrentState()){
+            case tookAccount -> "Sorry, but you can delete only categories in 'Income' and 'Outcome'";
+            case tookCategoryManager -> {
+                var deletedName = stateManager.getTakenCategoryManager().getCategories()
+                        .get(Integer.parseInt(splitterCommand[1])-1).getName();
+                stateManager.getTakenCategoryManager().removeCategoryWithIndex(Integer.parseInt(splitterCommand[1])-1);
+                yield String.format("you've deleted %s",deletedName);
+            }
+        };
+    }
+}
+
+class RenameCommand extends Command{
+    private String command;
+    public RenameCommand(StateManager stateManager, String command){
+        super(stateManager);
+        this.command = command;
+    }
+
+    @Override
+    public String execute() {
+        var splitterCommand = command.split(" ");
+        return switch (stateManager.getCurrentState()){
+            case tookAccount -> "Sorry, but you can rename only categories in 'Income' and 'Outcome'";
+            case tookCategoryManager -> {
+                var deletedName = stateManager.getTakenCategoryManager().getCategories()
+                        .get(Integer.parseInt(splitterCommand[1])-1).getName();
+                stateManager.getTakenCategoryManager().getCategories()
+                        .get(Integer.parseInt(splitterCommand[1])-1).setName(splitterCommand[2]);
+                var newName = stateManager.getTakenCategoryManager().getCategories()
+                        .get(Integer.parseInt(splitterCommand[1])-1).getName();
+                yield String.format("you've renamed %s into %s",deletedName, newName);
+            }
+        };
     }
 }
