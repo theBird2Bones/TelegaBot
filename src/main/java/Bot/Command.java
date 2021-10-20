ec6d6097fcadf536c793a5d72f5a40aab6b551f1
KoloskovaMariia <koloskovamariia@gmail.com>
2021-10-20 14:43:22 +0500

    написаны тесты для дерева

--- src/main/java/Bot/Command.java
+++ src/main/java/Bot/Command.java
@@ -226,17 +226,17 @@ class GetTreeCommand extends Command{
             if (categoryManager.getCategories().size() != 0) {
                 var i = 1;
                 for (var category : categoryManager.getCategories()) {
-                    tree += "        " + i + ") " + category.getName() + " : " + category.getTotal() + "\n";
+                    tree += "       " + i + ")" + category.getName() + ": " + category.getTotal() + "\n";
                     i++;
                 }
             } else{
                 tree += "       There are not any categories" + "\n";
             }
 
-            tree += "\n" + "    " +"Total: "+ categoryManager.getTotal() + "\n" + "\n";
+            tree += "\n" + "   " +"Total: "+ categoryManager.getTotal() + "\n" + "\n";
 
         }
-        tree += "Account Total: " + account.getTotal();
+        tree += "Account total: " + account.getTotal();
         return tree;
     }
 }
